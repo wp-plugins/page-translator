e33b35ce657f6c8630f71e2452cce1faa4824553
jiripolacek <jiripolacek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-07 19:14:13 +0000

    Plugin upgrade - better functionality.
    git-svn-id: https://plugins.svn.wordpress.org/page-translator/trunk@816583 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page-translator.php
+++ page-translator.php
@@ -3,13 +3,13 @@
 Plugin Name: Page Translator
 Plugin URI: http://www.letem.cz/page-translator/
 Description: Customize WordPress pagination URL. You can change 'page' to anything else. For example, 'http://www.yourwebsite.com/page/5/' -> 'http://www.yourwebsite.com/seite/5/'. After activating plugin you can set the new value in <a href="plugin-editor.php?file=page-translator/page-translator.php">Plugin Editor</a>. After actualization of the plugin file, don't forget to run <a href="options-permalink.php">Permalink Options</a> and click on 'Save changes' button. Now you can see your new - more friendly URLs :-)
-Version: 1.0.0
+Version: 1.1.0
 Author: Jiří Poláček
 Author URI: http://www.letem.cz/
 */
 
 
-$page_translator_new = 'seite'; // here is the new value instead of 'page', for example 'http://www.url.com/seite/5/'
+$page_translator_new = 'page'; // here type the new value instead of 'page', for example 'seite', so your URLs will be like 'http://www.url.com/seite/5/'
 
 
 add_action ('init', 'page_translator');
